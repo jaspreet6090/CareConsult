e55372796a5c8495330cf782b216bf30bb01e88a
MANMEET SINGH (AWM) <samanmeet85@gmail.com>
2023-05-29 18:18:01 +0530

    addingFiles

---
+++ assets/js/landing.js
@@ -0,0 +1,147 @@
+/* When your mouse cursor enter the background, the fading won't pause and keep playing */ 
+// $('.carousel').carousel({
+//     pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
+// });
+var counter = function () {
+  $("#section-counter, .hero-wrap, .ftco-counter").waypoint(
+    function (direction) {
+      if (
+        direction === "down" &&
+        !$(this.element).hasClass("ftco-animated")
+      ) {
+        var comma_separator_number_step =
+          $.animateNumber.numberStepFactories.separator(",");
+        $(".number").each(function () {
+          var $this = $(this),
+            num = $this.data("number");
+          console.log(num);
+          $this.animateNumber(
+            {
+              number: num,
+              numberStep: comma_separator_number_step,
+            },
+            7000
+          );
+        });
+      }
+    },
+    { offset: "95%" }
+  );
+};
+counter();
+
+var contentWayPoint = function () {
+  var i = 0;
+  $(".ftco-animate").waypoint(
+    function (direction) {
+      if (
+        direction === "down" &&
+        !$(this.element).hasClass("ftco-animated")
+      ) {
+        i++;
+
+        $(this.element).addClass("item-animate");
+        setTimeout(function () {
+          $("body .ftco-animate.item-animate").each(function (k) {
+            var el = $(this);
+            setTimeout(
+              function () {
+                var effect = el.data("animate-effect");
+                if (effect === "fadeIn") {
+                  el.addClass("fadeIn ftco-animated");
+                } 
+                // else if (effect === "fadeInLeft") {
+                  // el.addClass("fadeInLeft ftco-animated");
+                else if (effect === "fadeInRight") {
+                  el.addClass("fadeInRight ftco-animated");
+                } else {
+                  el.addClass("fadeInUp ftco-animated");
+                }
+                el.removeClass("item-animate");
+              },
+              k * 50,
+              "easeInOutExpo"
+            );
+          });
+        }, 100);
+      }
+    },
+    { offset: "95%" }
+  );
+};
+
+
+
+contentWayPoint();
+
+
+$(document).ready(function () {
+    $(".fadeInleft-animate").waypoint(function(direction){
+      $(".fadeInleft-animate").addClass("animated fadeInLeft")
+  
+    },{
+      offset:"50%"
+    });
+    $(".fadeInRight-animate").waypoint(function(direction){
+      $(".fadeInRight-animate").addClass("animated fadeInRight")
+  
+    },{
+      offset:"50%"
+    });
+    $(".zoomInDown-animate").waypoint(function(direction){
+      $(".zoomInDown-animate").addClass("animated zoomInDown")
+  
+    },{
+      offset:"50%"
+    });
+    $(".zoomIn-animate").waypoint(function(direction){
+      $(".zoomIn-animate").addClass("animated zoomIn")
+  
+    },{
+      offset:"50%"
+    });
+    $(".zoomInDown-animate").waypoint(function(direction){
+      $(".zoomInDown-animate").addClass("animated zoomInDown")
+  
+    },{
+      offset:"50%"
+    });
+    $(".fadeInUp-animate").waypoint(function(direction){
+      $(".fadeInUp-animate").addClass("animated fadeInUp")
+  
+    },{
+      offset:"50%"
+    });
+    $(".rotateInDownRight-animate").waypoint(function(direction){
+      $(".rotateInDownRight-animate").addClass("animated rotateInDownRight")
+  
+    },{
+      offset:"50%"
+    });
+    $(".rotateInDownLeft-animate").waypoint(function(direction){
+      $(".rotateInDownLeft-animate").addClass("animated rotateInDownLeft")
+  
+    },{
+      offset:"50%"
+    });
+    $(".flash-animate").waypoint(function(direction){
+      $(".flash-animate").addClass("animated flash")
+  
+    },{
+      offset:"50%"
+    });
+    $(".flipInX-animate").waypoint(function(direction){
+      $(".flipInX-animate").addClass("animated flipInX")
+  
+    },{
+      offset:"50%"
+    });
+  
+  });
+  
+
+
+  function toggleShow () {
+    var el = document.getElementById("box");
+    el.classList.toggle("show");
+  }
---
+++ js/main.js
@@ -0,0 +1,618 @@
+(function ($) {
+  "use strict";
+
+  $(window).stellar({
+    responsive: true,
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,
+    scrollProperty: "scroll",
+  });
+
+  var fullHeight = function () {
+    $(".js-fullheight").css("height", $(window).height());
+    $(window).resize(function () {
+      $(".js-fullheight").css("height", $(window).height());
+    });
+  };
+  fullHeight();
+
+  // loader
+  var loader = function () {
+    setTimeout(function () {
+      if ($("#ftco-loader").length > 0) {
+        $("#ftco-loader").removeClass("show");
+      }
+    }, 1);
+  };
+  loader();
+
+  // Scrollax
+  $.Scrollax();
+
+  // Burger Menu
+  var burgerMenu = function () {
+    $("body").on("click", ".js-fh5co-nav-toggle", function (event) {
+      event.preventDefault();
+
+      if ($("#ftco-nav").is(":visible")) {
+        $(this).removeClass("active");
+      } else {
+        $(this).addClass("active");
+      }
+    });
+  };
+  burgerMenu();
+
+  var onePageClick = function () {
+    $(document).on("click", '#ftco-nav a[href^="#"]', function (event) {
+      event.preventDefault();
+
+      var href = $.attr(this, "href");
+
+      $("html, body").animate(
+        {
+          scrollTop: $($.attr(this, "href")).offset().top - 70,
+        },
+        500,
+        function () {
+          // window.location.hash = href;
+        }
+      );
+    });
+  };
+
+  onePageClick();
+
+  var carousel = function () {
+    $(".home-slider").owlCarousel({
+      loop: true,
+      autoplay: true,
+      margin: 0,
+      animateOut: "fadeOut",
+      animateIn: "fadeIn",
+      nav: false,
+      autoplayHoverPause: false,
+      items: 1,
+      navText: [
+        "<span class='ion-md-arrow-back'></span>",
+        "<span class='ion-chevron-right'></span>",
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 1,
+        },
+        1000: {
+          items: 1,
+        },
+      },
+    });
+    $(".carousel-testimony").owlCarousel({
+      center: true,
+      loop: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      items: 1,
+      margin: 30,
+      stagePadding: 0,
+      nav: false,
+      navText: [
+        '<span class="ion-ios-arrow-back">',
+        '<span class="ion-ios-arrow-forward">',
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+      },
+    });
+  };
+  carousel();
+
+  $("nav .dropdown").hover(
+    function () {
+      var $this = $(this);
+      // 	 timer;
+      // clearTimeout(timer);
+      $this.addClass("show");
+      $this.find("> a").attr("aria-expanded", true);
+      // $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
+      $this.find(".dropdown-menu").addClass("show");
+    },
+    function () {
+      var $this = $(this);
+      // timer;
+      // timer = setTimeout(function(){
+      $this.removeClass("show");
+      $this.find("> a").attr("aria-expanded", false);
+      // $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
+      $this.find(".dropdown-menu").removeClass("show");
+      // }, 100);
+    }
+  );
+
+  $("#dropdown04").on("show.bs.dropdown", function () {
+    console.log("show");
+  });
+
+  // scroll
+  var scrollWindow = function () {
+    $(window).scroll(function () {
+      var $w = $(this),
+        st = $w.scrollTop(),
+        navbar = $(".ftco_navbar"),
+        sd = $(".js-scroll-wrap");
+
+      if (st > 150) {
+        if (!navbar.hasClass("scrolled")) {
+          navbar.addClass("scrolled");
+        }
+      }
+      if (st < 150) {
+        if (navbar.hasClass("scrolled")) {
+          navbar.removeClass("scrolled sleep");
+        }
+      }
+      if (st > 350) {
+        if (!navbar.hasClass("awake")) {
+          navbar.addClass("awake");
+        }
+
+        if (sd.length > 0) {
+          sd.addClass("sleep");
+        }
+      }
+      if (st < 350) {
+        if (navbar.hasClass("awake")) {
+          navbar.removeClass("awake");
+          navbar.addClass("sleep");
+        }
+        if (sd.length > 0) {
+          sd.removeClass("sleep");
+        }
+      }
+    });
+  };
+  scrollWindow();
+
+  var counter = function () {
+    $("#section-counter, .hero-wrap, .ftco-counter").waypoint(
+      function (direction) {
+        if (
+          direction === "down" &&
+          !$(this.element).hasClass("ftco-animated")
+        ) {
+          var comma_separator_number_step =
+            $.animateNumber.numberStepFactories.separator(",");
+          $(".number").each(function () {
+            var $this = $(this),
+              num = $this.data("number");
+            console.log(num);
+            $this.animateNumber(
+              {
+                number: num,
+                numberStep: comma_separator_number_step,
+              },
+              7000
+            );
+          });
+        }
+      },
+      { offset: "95%" }
+    );
+  };
+  counter();
+
+  var contentWayPoint = function () {
+    var i = 0;
+    $(".ftco-animate").waypoint(
+      function (direction) {
+        if (
+          direction === "down" &&
+          !$(this.element).hasClass("ftco-animated")
+        ) {
+          i++;
+
+          $(this.element).addClass("item-animate");
+          setTimeout(function () {
+            $("body .ftco-animate.item-animate").each(function (k) {
+              var el = $(this);
+              setTimeout(
+                function () {
+                  var effect = el.data("animate-effect");
+                  if (effect === "fadeIn") {
+                    el.addClass("fadeIn ftco-animated");
+                  } 
+                  // else if (effect === "fadeInLeft") {
+                    // el.addClass("fadeInLeft ftco-animated");
+                  else if (effect === "fadeInRight") {
+                    el.addClass("fadeInRight ftco-animated");
+                  } else {
+                    el.addClass("fadeInUp ftco-animated");
+                  }
+                  el.removeClass("item-animate");
+                },
+                k * 50,
+                "easeInOutExpo"
+              );
+            });
+          }, 100);
+        }
+      },
+      { offset: "95%" }
+    );
+  };
+
+
+  
+  contentWayPoint();
+
+  // magnific popup
+  $(".image-popup").magnificPopup({
+    type: "image",
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: "mfp-no-margins mfp-with-zoom", // class to remove default margin from left and right side
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      verticalFit: true,
+    },
+    zoom: {
+      enabled: true,
+      duration: 300, // don't foget to change the duration also in CSS
+    },
+  });
+
+  $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+    disableOn: 700,
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false,
+  });
+})(jQuery);
+
+$(function () {
+  $(".progress").each(function () {
+    var value = $(this).attr("data-value");
+    var left = $(this).find(".progress-left .progress-bar");
+    var right = $(this).find(".progress-right .progress-bar");
+
+    if (value > 0) {
+      if (value <= 50) {
+        right.css("transform", "rotate(" + percentageToDegrees(value) + "deg)");
+      } else {
+        right.css("transform", "rotate(180deg)");
+        left.css(
+          "transform",
+          "rotate(" + percentageToDegrees(value - 50) + "deg)"
+        );
+      }
+    }
+  });
+
+  function percentageToDegrees(percentage) {
+    return (percentage / 100) * 360;
+  }
+});
+
+// adding autotyping to the UI over here
+let typed = new Typed("#auto-input", {
+  strings: [
+    "Data Enthusiast",
+    "Problem Solver",
+    "AI Enthusiast",
+    "Python Lover",
+    "Innovative",
+    "Creative",
+    "Fast Learner",
+    "Self Motivated",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+/*--------------------
+Vars
+--------------------*/
+let progress = 50;
+let startX = 0;
+let active = 0;
+let isDown = false;
+
+/*--------------------
+Contants
+--------------------*/
+const speedWheel = 0.02;
+const speedDrag = -0.1;
+
+/*--------------------
+Get Z
+--------------------*/
+const getZindex = (array, index) =>
+  array.map((_, i) =>
+    index === i ? array.length : array.length - Math.abs(index - i)
+  );
+
+/*--------------------
+Items
+--------------------*/
+const $items = document.querySelectorAll(".carousel-item");
+const $cursors = document.querySelectorAll(".cursor");
+
+const displayItems = (item, index, active) => {
+  const zIndex = getZindex([...$items], active)[index];
+  item.style.setProperty("--zIndex", zIndex);
+  item.style.setProperty("--active", (index - active) / $items.length);
+};
+
+/*--------------------
+Animate
+--------------------*/
+const animate = () => {
+  progress = Math.max(0, Math.min(progress, 100));
+  active = Math.floor((progress / 100) * ($items.length - 1));
+
+  $items.forEach((item, index) => displayItems(item, index, active));
+};
+animate();
+
+/*--------------------
+Click on Items
+--------------------*/
+$items.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    progress = (i / $items.length) * 100 + 10;
+    animate();
+  });
+});
+
+/*--------------------
+Handlers
+--------------------*/
+const handleWheel = (e) => {
+  const wheelProgress = e.deltaY * speedWheel;
+  progress = progress + wheelProgress;
+  animate();
+};
+
+const handleMouseMove = (e) => {
+  if (e.type === "mousemove") {
+    $cursors.forEach(($cursor) => {
+      $cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+    });
+  }
+  if (!isDown) return;
+  const x = e.clientX || (e.touches && e.touches[0].clientX) || 0;
+  const mouseProgress = (x - startX) * speedDrag;
+  progress = progress + mouseProgress;
+  startX = x;
+  animate();
+};
+
+const handleMouseDown = (e) => {
+  isDown = true;
+  startX = e.clientX || (e.touches && e.touches[0].clientX) || 0;
+};
+
+const handleMouseUp = () => {
+  isDown = false;
+};
+
+/*--------------------
+Listeners
+--------------------*/
+document.addEventListener("mousewheel", handleWheel);
+document.addEventListener("mousedown", handleMouseDown);
+document.addEventListener("mousemove", handleMouseMove);
+document.addEventListener("mouseup", handleMouseUp);
+document.addEventListener("touchstart", handleMouseDown);
+document.addEventListener("touchmove", handleMouseMove);
+document.addEventListener("touchend", handleMouseUp);
+
+const swiper = new Swiper(".swiper-slider", {
+  // Optional parameters
+  centeredSlides: true,
+  slidesPerView: 1,
+  grabCursor: true,
+  freeMode: false,
+  loop: true,
+  mousewheel: false,
+  keyboard: {
+    enabled: true,
+  },
+
+  // Enabled autoplay mode
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: false,
+    clickable: true,
+  },
+
+  // If we need navigation
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    640: {
+      slidesPerView: 1.25,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+});
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+    $(".scrolltop:hidden").stop(true, true).fadeIn();
+  } else {
+    $(".scrolltop").stop(true, true).fadeOut();
+  }
+});
+$(function () {
+  $(".scroll").click(function () {
+    $("html,body").animate({ scrollTop: $(".top").offset().top }, "1000");
+    return false;
+  });
+});
+
+
+
+// adding way point offsets code over here
+// var waypoint = new Waypoint({
+//   element: document.getElementById('px-offset-waypoint'),
+//   handler: function(direction) {
+//     notify('I am 20px from the top of the window')
+//   },
+//   offset: 20 
+// })
+
+
+
+$(document).ready(function () {
+  $(".fadeInleft-animate").waypoint(function(direction){
+    $(".fadeInleft-animate").addClass("animated fadeInLeft")
+
+  },{
+    offset:"50%"
+  });
+  $(".fadeInRight-animate").waypoint(function(direction){
+    $(".fadeInRight-animate").addClass("animated fadeInRight")
+
+  },{
+    offset:"50%"
+  });
+  $(".zoomInDown-animate").waypoint(function(direction){
+    $(".zoomInDown-animate").addClass("animated zoomInDown")
+
+  },{
+    offset:"50%"
+  });
+  $(".zoomIn-animate").waypoint(function(direction){
+    $(".zoomIn-animate").addClass("animated zoomIn")
+
+  },{
+    offset:"50%"
+  });
+  $(".zoomInDown-animate").waypoint(function(direction){
+    $(".zoomInDown-animate").addClass("animated zoomInDown")
+
+  },{
+    offset:"50%"
+  });
+  $(".fadeInUp-animate").waypoint(function(direction){
+    $(".fadeInUp-animate").addClass("animated fadeInUp")
+
+  },{
+    offset:"50%"
+  });
+  $(".rotateInDownRight-animate").waypoint(function(direction){
+    $(".rotateInDownRight-animate").addClass("animated rotateInDownRight")
+
+  },{
+    offset:"50%"
+  });
+  $(".rotateInDownLeft-animate").waypoint(function(direction){
+    $(".rotateInDownLeft-animate").addClass("animated rotateInDownLeft")
+
+  },{
+    offset:"50%"
+  });
+  $(".flash-animate").waypoint(function(direction){
+    $(".flash-animate").addClass("animated flash")
+
+  },{
+    offset:"50%"
+  });
+  $(".flipInX-animate").waypoint(function(direction){
+    $(".flipInX-animate").addClass("animated flipInX")
+
+  },{
+    offset:"50%"
+  });
+
+
+
+  
+
+
+
+
+});
+
+
+
+
+// this is the code over here to add
+//  var contentWayPoint = function () {
+//   var i = 0;
+//   $(".ftco-animate").waypoint(
+//     function (direction) {
+//       if (
+//         direction === "down" &&
+//         !$(this.element).hasClass("ftco-animated")
+//       ) {
+//         i++;
+
+//         $(this.element).addClass("item-animate");
+//         setTimeout(function () {
+//           $("body .ftco-animate.item-animate").each(function (k) {
+//             var el = $(this);
+//             setTimeout(
+//               function () {
+//                 var effect = el.data("animate-effect");
+//                 if (effect === "fadeIn") {
+//                   el.addClass("fadeIn ftco-animated");
+//                 } else if (effect === "fadeInLeft") {
+//                   el.addClass("fadeInLeft ftco-animated");
+//                 } else if (effect === "fadeInRight") {
+//                   el.addClass("fadeInRight ftco-animated");
+//                 } else {
+//                   el.addClass("fadeInUp ftco-animated");
+//                 }
+//                 el.removeClass("item-animate");
+//               },
+//               k * 50,
+//               "easeInOutExpo"
+//             );
+//           });
+//         }, 100);
+//       }
+//     },
+//     { offset: "95%" }
+//   );
+// };
+
+
+
+// contentWayPoint();
